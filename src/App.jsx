d91eb5a978c9a98fe8837fcc71f2031905fa049f
Rohit <rohitbhagat9302@gmail.com>
2024-02-20 22:30:27 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -23,9 +23,6 @@ function App() {
         {/* <Route path='project'element={<Project/>} /> */}
         <Route path='portfolio'element={<Portfolio/>} />
       </Routes>
-
-
-
     </BrowserRouter>
   )
 }
