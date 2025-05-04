6a4dd68913cb095394f839c15b523488e60e018c
Rohit bhagat <rohitbhagat@Rohits-MacBook-Air.local>
2025-05-04 13:00:55 +0530

    enhncement of my portfolit

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,38 +1,39 @@
-import React from 'react';
-import  profile from "../../assets/rohit.jpg"
-import { Link } from 'react-router-dom';
-import {FaArrowRight} from 'react-icons/fa'
-import "./home.css"
+import React from "react";
+import profile from "/public//personal.jpg";
+import { Link } from "react-router-dom";
+import { FaArrowRight } from "react-icons/fa";
+import "./home.css";
 
 function Home() {
   return (
-    <section className='home section grid'>
-      <img src={profile} className='home__img' />
+    <section className="home section grid">
+      <img src={profile} className="home__img" />
 
-      <div className='home__content'>
-      <div className='home__data'>
-         <h1 className='home__title'>
-          <span>I'm Rohit Bhagat.</span> Web Developer
-         </h1>
+      <div className="home__content">
+        <div className="home__data">
+          <h1 className="home__title">
+            <span>I'm Rohit Bhagat.</span>{" "}
+            <p className="text-2xl">Growth-Focused Product Engineer</p>
+          </h1>
 
-         <p className='home__description'>
-          I'm a Indian based web developer & front-end developer focused on 
-          crafting clean & user friendly experiences, I am passionate about 
-          building excellent software that improves the lives of those around me.
-         </p>
+          <p className="home__description">
+            I'm a Product Engineer based in India, focused on building scalable,
+            user-centric solutions that bridge technology and business needs. I
+            thrive on turning complex problems into elegant products, driving
+            growth through thoughtful engineering, design collaboration, and a
+            deep understanding of user behavior.
+          </p>
 
-         <Link to='/about' className='button'>
-         More About me{' '}
-         <span className='button__icon'>
-          <FaArrowRight />
-         </span>
-         </Link>
+          <Link to="/about" className="button">
+            More About me{" "}
+            <span className="button__icon">
+              <FaArrowRight />
+            </span>
+          </Link>
+        </div>
       </div>
-      </div>
 
-      <div className='color__block'>
-
-      </div>
+      <div className="color__block"></div>
     </section>
   );
 }
--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: "/My-Portfolio/"
+  plugins: [react(),   tailwindcss(),],
+  base: "/My-Portfolio"
 })
